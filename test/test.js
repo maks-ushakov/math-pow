9dd86b3de89812daf87185dce8ad783dc9095ec2
Maks Ushakov <ush.maksim@gmail.com>
2017-02-08 14:18:43 +0200

    Improve tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 describe("pow", function() {
 
-  it("возводит в n-ю степень", function() {
+  it("При возведении 2 в 3ю степень результат 8", function() {
     assert.equal(pow(2, 3), 8);
+  });
+  it("При возведении 3 в 4ю степень результат 81", function() {
+    assert.equal(pow(3, 4), 81);
   });
 
 });
