4813bd6bf6dce326c4cba691412d400d7e11183a
Maks Ushakov <ush.maksim@gmail.com>
2017-02-08 15:11:58 +0200

    Change test. Test for positive integer digit is lop now

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
 describe("pow", function() {
+	function makeTest(x) {
+		var expected = x * x * x;
+		it("При возведении " + x + 
+			" в 3ю степень результат равен " + expected, function() {
+			assert.equal(pow(x, 3), expected);
+		});
+	}
 
-  it("При возведении 2 в 3ю степень результат 8", function() {
-    assert.equal(pow(2, 3), 8);
-  });
-  it("При возведении 3 в 4ю степень результат 81", function() {
-    assert.equal(pow(3, 4), 81);
-  });
+	for (var i=1; i <= 5; i++) {
+		makeTest(i);
+	}
 
 });
