aaaca85f5c0f3fadbda229eb56661e0be57866a3
Maks Ushakov <ush.maksim@gmail.com>
2017-02-08 15:47:45 +0200

    Add 'zero power case' to text

--- test/test.js
+++ test/test.js
@@ -22,4 +22,22 @@ describe("pow", function() {
 	    	assert(isNaN(pow(2, 1.5)), "pow(2, 1.5) не NaN");
 	  	});
 	 });
+
+	describe("любое число, кроме нуля, в степени 0 равно 1", function() {
+
+	    function makeTest(x) {
+	      it("при возведении " + x + " в степень 0 результат: 1", function() {
+	        assert.equal(pow(x, 0), 1);
+	      });
+	    }
+
+	    for (var x = -5; x <= 5; x += 2) {
+	      makeTest(x);
+	    }
+
+	 });
+
+	it("ноль в нулевой степени даёт NaN", function() {
+	    assert(isNaN(pow(0, 0)), "0 в степени 0 не NaN");
+	});
 });
