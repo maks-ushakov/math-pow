7fbf24271e4ab4374aa94df09596b9bb348e11ef
Maks Ushakov <ush.maksim@gmail.com>
2017-02-08 15:20:34 +0200

    Change test. Separate loop test for pozitive digit

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 describe("pow", function() {
-	function makeTest(x) {
-		var expected = x * x * x;
-		it("При возведении " + x + 
-			" в 3ю степень результат равен " + expected, function() {
-			assert.equal(pow(x, 3), expected);
-		});
-	}
+	describe("Возводит х в степень 3", function() {
+		function makeTest(x) {
+			var expected = x * x * x;
+			it("При возведении " + x + 
+				" в 3ю степень результат равен " + expected, function() {
+				assert.equal(pow(x, 3), expected);
+			});
+		}
 
-	for (var i=1; i <= 5; i++) {
-		makeTest(i);
-	}
+		for (var i=1; i <= 5; i++) {
+			makeTest(i);
+		}
+	});
 
 });
